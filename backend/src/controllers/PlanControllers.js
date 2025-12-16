6e804ebb57c301bfc00bac409b68e936fd54d8b1
clivedev88m <clive.dev88@gmail.com>
2025-12-16 17:28:43 -0300

    configuração do banco de dados sqlite

--- backend/src/controllers/PlanControllers.js
+++ backend/src/controllers/PlanControllers.js
@@ -1,9 +1,13 @@
 const PlanModel = require('../models/PlanModel')
-
+const db = require('../config/db');
 class PlanControllers {
     static generatePlan(req, res) {
         const {theme} = req.body;
         const plan = PlanModel.generatePlan(theme);
+
+        db.prepare('INSERT INTO plans (theme, plan) VALUES (?, ?)'
+        ).run(theme, JSON.stringify(plan)
+        );
 
         res.status(200).json({
             tema:theme,
